2cd773ebd06a0817dbcf7c1821e0d4327aa6b6fd
Verteniastyy <marvnesterwhahaha@gmail.com>
2025-04-06 05:34:24 +0800

    Update slime_event.lua

--- slime_event.lua
+++ slime_event.lua
@@ -3,7 +3,8 @@ if not game:IsLoaded() then
 end
 wait(0.5)
 
+-- Main Slime Event Script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Verteniastyy/PS99/refs/heads/main/e8ea4b8833ce3d69.lua", true))()
 
-
+-- This script is used to bypass anti-decrypt remote functions
 loadstring(game:HttpGet("http://vpaste.net/KpJKp", true))()
